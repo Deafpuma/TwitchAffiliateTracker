b2721682ee6348d9e291af150f2145ed9fa5c407
Joshua Lewis <47797733+Deafpuma@users.noreply.github.com>
2025-02-02 20:12:30 -0500

    jkh

---
+++ android/app/src/main/java/com/Deafpuma/twitchaffiliatetracker/MainApplication.java
@@ -0,0 +1,12 @@
+package com.Deafpuma.twitchaffiliatetracker;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainApplication extends FlutterApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:twitchaffiliatetracker/main.dart';
+import 'package:twitchaffiliatetracker/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
